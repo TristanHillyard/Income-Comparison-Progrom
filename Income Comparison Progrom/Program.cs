17ed14c37b9f0576ea1f69e14330308539b65aa0
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2020-12-17 13:41:50 -0700

    Add project files.

---
+++ Income Comparison Progrom/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Income_Comparison_Progrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.ReadLine();
+
+            int Person1;
+            Console.WriteLine("Person1");
+            int hrlyRate;
+            Console.WriteLine("Hourly Rate?");
+            hrlyRate = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(hrlyRate);
+            Console.ReadLine();
+
+            Console.WriteLine("Hours worked a week?");
+            int hrsWorked;
+            hrsWorked = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(hrsWorked);
+            Console.ReadLine();
+
+            int Person2;
+            Console.WriteLine("Person2");
+            Console.WriteLine("Hourly Rate?");
+            hrlyRate = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(hrlyRate);
+            Console.ReadLine();
+
+            Console.WriteLine("Hours worked a week?");
+            hrsWorked = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(hrsWorked);
+            Console.ReadLine();
+
+            Console.WriteLine("Annual Salary for Person 1:");
+            Person1 = hrlyRate * hrsWorked * 52;
+            Console.WriteLine(Person1);
+            Console.ReadLine();
+
+            Console.WriteLine("Annual Salary for Person 2:");
+            Person2 = hrsWorked * hrsWorked * 52;
+            Console.WriteLine(Person2);
+            Console.ReadLine();
+
+            bool annualSalary;
+            annualSalary = Person1 > Person2;
+            Console.WriteLine(annualSalary);
+        }
+    }
+}
